9274fb21a0fb596263f934a0c43a914db31fff8c
MiguelARam27 <garandster27@live.com>
2021-07-16 21:33:02 -0400

    signup, cloudinary fix

--- pages/signup.js
+++ pages/signup.js
@@ -103,6 +103,7 @@ function Signup() {
       setFormLoading(false);
       return setErrorMsg('Error Uploading Image');
     }
+    console.log(profilePicUrl);
 
     await registerUser(user, profilePicUrl, setErrorMsg, setFormLoading);
   };
