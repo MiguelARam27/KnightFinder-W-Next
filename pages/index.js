68ab2b7cd0db9ea76a07db06f973fdb2330af244
MiguelARam27 <garandster27@live.com>
2021-07-17 22:57:06 -0400

    User login with cookies

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import baseUrl from '../utils/baseUrl';
 
-function Index({ posts }) {
-  return (
-    <div>
-      {posts &&
-        posts?.length > 0 &&
-        posts.map((post) => <h1 key={post._id}>{post.title}</h1>)}
-    </div>
-  );
+function Index({ user, userFollowStats }) {
+  console.log(user, userFollowStats);
+  return <div></div>;
 }
 Index.getInitialProps = async (ctx) => {
   try {
