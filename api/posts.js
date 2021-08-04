a0e72b7259c56de671c23feec9cad6a408c387cf
MiguelARam27 <garandster27@live.com>
2021-08-04 11:36:02 -0400

    Infinite Scroll for posts

--- api/posts.js
+++ api/posts.js
@@ -33,18 +33,38 @@ router.post('/', authMiddleware, async (req, res) => {
 
 //Get all posts
 router.get('/', authMiddleware, async (req, res) => {
+  const { pageNumber } = req.query;
+
+  const number = Number(pageNumber);
+  const size = 8;
+
   try {
-    const posts = await PostModel.find()
-      .sort({ createdAt: -1 })
-      .populate('user')
-      .populate('comments.user');
+    let posts;
+
+    if (number === 1) {
+      posts = await PostModel.find()
+        .limit(size)
+        .sort({ createdAt: -1 })
+        .populate('user')
+        .populate('comments.user');
+    }
+    //
+    else {
+      const skips = size * (number - 1);
+      posts = await PostModel.find()
+        .skip(skips)
+        .limit(size)
+        .sort({ createdAt: -1 })
+        .populate('user')
+        .populate('comments.user');
+    }
+
     return res.json(posts);
   } catch (error) {
     console.error(error);
     return res.status(500).send(`Server error`);
   }
 });
-
 //Get post by id
 router.get('/:postId', authMiddleware, async (req, res) => {
   try {
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,19 @@ import CardPost from '../components/post/CardPost';
 import { NoPosts } from '../components/Layout/NoData';
 import { parseCookies } from 'nookies';
 import { Segment } from 'semantic-ui-react';
-
 import { PostDeleteToastr } from '../components/Layout/Toastr';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import {
+  PlaceHolderPosts,
+  EndMessage,
+} from '../components/Layout/PlaceHolderGroup';
+import cookie from 'js-cookie';
 function Index({ user, postsData, errorLoading }) {
-  const [posts, setPosts] = useState(postsData);
+  const [posts, setPosts] = useState(postsData || []);
   const [showToastr, setShowToastr] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const [pageNumber, setPageNumber] = useState(2);
   useEffect(() => {
     document.title = `Welcome, ${user.name.split(' ')[0]}`;
   }, []);
@@ -21,14 +29,36 @@ function Index({ user, postsData, errorLoading }) {
   if (posts.length === 0 || errorLoading) {
     return <NoPosts />;
   }
+
+  const fetchDataOnScroll = async () => {
+    try {
+      const res = await axios.get(`${baseUrl}/api/posts`, {
+        headers: { Authorization: cookie.get('token') },
+        params: { pageNumber },
+      });
+      if (res.data.length === 0) {
+        setHasMore(false);
+      }
+
+      setPosts((prev) => [...prev, ...res.data]);
+    } catch (error) {
+      alert('Error Fetching Posts');
+    }
+  };
   return (
     <>
       {showToastr && <PostDeleteToastr />}
       <Segment>
         <CreatePost user={user} setPosts={setPosts} />
 
-        {posts.map((post) => {
-          return (
+        <InfiniteScroll
+          hasMore={hasMore}
+          next={fetchDataOnScroll}
+          loader={<PlaceHolderPosts />}
+          endMessage={<EndMessage />}
+          dataLength={posts.length}
+        >
+          {posts.map((post) => (
             <CardPost
               key={post._id}
               post={post}
@@ -36,8 +66,8 @@ function Index({ user, postsData, errorLoading }) {
               setPosts={setPosts}
               setShowToastr={setShowToastr}
             />
-          );
-        })}
+          ))}
+        </InfiniteScroll>
       </Segment>
     </>
   );
@@ -48,7 +78,9 @@ Index.getInitialProps = async (ctx) => {
 
     const res = await axios.get(`${baseUrl}/api/posts`, {
       headers: { Authorization: token },
+      params: { pageNumber: 1 },
     });
+
     return { postsData: res.data };
   } catch (error) {
     return { errorLoading: true };
