1a71770c5cf7e0e2d354f476cbde7109ff34f6fd
MiguelARam27 <garandster27@live.com>
2021-08-06 10:28:12 -0400

    get profile info, get posts of user

---
+++ api/profile.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const UserModel = require('../models/UserModel');
+const FollowerModel = require('../models/FollowerModel');
+const PostModel = require('../models/PostModal');
+const ProfileModel = require('../models/ProfileModel');
+//Get Profile Info
+router.get('/:username', authMiddleware, async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await UserModel.findOne({ username: username.toLowerCase() });
+    if (!user) {
+      return res.status(404).send('user not found');
+    }
+    const profile = await ProfileModel.findOne({ user: user._id }).populate(
+      'user'
+    );
+    const profileFollowStats = await FollowerModel.findOne({ user: user._id });
+    return res.json({
+      profile,
+      followersLength:
+        profileFollowStats.followers.length > 0
+          ? profileFollowStats.followers.length
+          : 0,
+      followingLenght:
+        profileFollowStats.following.length > 0
+          ? profileFollowStats.following.length
+          : 0,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
+
+//Get user post
+
+router.get('/posts/:username', authMiddleware, async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await UserModel.findOne({ username: username.toLowerCase() });
+    if (!user) {
+      return res.status(404).send('user not found');
+    }
+    const posts = await PostModel.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate('user')
+      .populate('comments');
+
+    return res.json(posts);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
+module.exports = router;
