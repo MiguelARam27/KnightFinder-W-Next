2f6f65e519e9f8202599b7cdc7797220b96f4702
MiguelARam27 <garandster27@live.com>
2021-11-14 22:30:31 -0500

    Home menu css changes

--- components/Layout/home/SideMenu.js
+++ components/Layout/home/SideMenu.js
@@ -15,96 +15,44 @@ function SideMenu({
     <>
       <div className={styles.sidebar}>
         <ul>
-          <li>
-            <i className="home large icon"></i>
-            <span>Home</span>
-          </li>
-          <li>
-            <i className="mail large icon"></i>
-            <span>Messages</span>
-          </li>
-          <li>
-            <i className="bell large icon"></i>
-            <span>Notifications</span>
-          </li>
-          <li>
-            <i className="user large icon"></i>
-            <span>Account</span>
-          </li>
-          <li>
+          <Link href="/home">
+            <li className={isActive('/home') ? `${styles.link_active}` : null}>
+              <i className="home large icon "></i>
+              <span>Home</span>
+            </li>
+          </Link>
+          <Link href="/messages">
+            <li
+              className={isActive('/messages') ? `${styles.link_active}` : null}
+            >
+              <i className="mail large icon"></i>
+              <span>Messages</span>
+            </li>
+          </Link>
+          <Link href="/notifications">
+            <li
+              className={
+                isActive('/notifications') ? `${styles.link_active}` : null
+              }
+            >
+              <i className="bell large icon"></i>
+              <span>Notifications</span>
+            </li>
+          </Link>
+
+          <Link href={`/user/${username}`}>
+            <li
+              className={isActive('/account') ? `${styles.link_active}` : null}
+            >
+              <i className="user large icon"></i>
+              <span>Account</span>
+            </li>
+          </Link>
+          <li onClick={() => logoutUser(email)}>
             <i className="log out large icon"></i>
             <span>Logout</span>
           </li>
         </ul>
-        {/* <List
-          style={{ paddingTop: '1rem' }}
-          size="big"
-          verticalAlign="middle"
-          selection
-        >
-          <Link href="/">
-            <List.Item active={isActive('/')}>
-              <Icon
-                name="home"
-                size="large"
-                color={isActive('/') ? 'teal' : 'black'}
-              />
-              <List.Content>
-                <List.Header content="Home" />
-              </List.Content>
-            </List.Item>
-          </Link>
-          <br />
-
-          <Link href="/messages">
-            <List.Item active={isActive('/messages')}>
-              <Icon
-                name={unreadMessage ? 'hand point right' : 'mail outline'}
-                size="large"
-                color={isActive('/') ? 'teal' : 'black'}
-              />
-              <List.Content>
-                <List.Header content="Messages" />
-              </List.Content>
-            </List.Item>
-          </Link>
-          <br />
-
-          <Link href="/notifications">
-            <List.Item active={isActive('/notifications')}>
-              <Icon
-                name={unreadNotification ? 'hand point right' : 'bell outline'}
-                size="large"
-                color={isActive('/') ? 'teal' : 'black'}
-              />
-              <List.Content>
-                <List.Header content="Notifications" />
-              </List.Content>
-            </List.Item>
-          </Link>
-          <br />
-
-          <Link href={`/${username}`}>
-            <List.Item active={router.query.username === username}>
-              <Icon
-                name="user"
-                size="large"
-                color={router.query.username === username ? 'teal' : 'black'}
-              />
-              <List.Content>
-                <List.Header content="Account" />
-              </List.Content>
-            </List.Item>
-          </Link>
-          <br />
-
-          <List.Item onClick={() => logoutUser(email)}>
-            <Icon name="log out" size="large" />
-            <List.Content>
-              <List.Header content="Logout" />
-            </List.Content>
-          </List.Item>
-        </List> */}
       </div>
     </>
   );
