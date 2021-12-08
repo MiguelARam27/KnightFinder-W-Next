01c3094b9f37b294ae9856916a656fa2521f9e01
MiguelARam27 <garandster27@live.com>
2021-12-08 13:25:27 -0500

    follower notification css changes

--- components/Notifications/FollowerNotification.js
+++ components/Notifications/FollowerNotification.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Feed, Button, Divider } from 'semantic-ui-react';
 import calculateTime from '../../utils/calculatedTime';
 import { followUser, unfollowUser } from '../../utils/profileActions';
+import styles from '@/styles/NotificationsPage.module.scss';
 
 function FollowerNotification({
   notification,
@@ -18,42 +19,22 @@ function FollowerNotification({
 
   return (
     <>
-      <Feed.Event>
-        <Feed.Label image={notification.user.profilePicUrl} />
-        <Feed.Content>
-          <Feed.Summary>
-            <>
-              <Feed.User as="a" href={`/${notification.user.username}`}>
-                {notification.user.name}
-              </Feed.User>{' '}
-              started following you.
-              <Feed.Date>{calculateTime(notification.date)}</Feed.Date>
-            </>
-          </Feed.Summary>
-
-          <div style={{ position: 'absolute', right: '5px' }}>
-            <Button
-              size="small"
-              compact
-              icon={isFollowing ? 'check circle' : 'add user'}
-              color={isFollowing ? 'instagram' : 'twitter'}
-              disabled={disabled}
-              onClick={async () => {
-                setDisabled(true);
-
-                isFollowing
-                  ? await unfollowUser(
-                      notification.user._id,
-                      setUserFollowStats
-                    )
-                  : await followUser(notification.user._id, setUserFollowStats);
-
-                setDisabled(false);
-              }}
-            />
+      <div className="event">
+        <div className="label">
+          <img src={notification.user.profilePicUrl} alt="Profile pic" />
+        </div>
+        <div className="content">
+          <div className="summary">
+            <a className={styles.link} href={`/${notification.user.username}`}>
+              {' '}
+              {notification.user.name}{' '}
+            </a>
+            started following you.
+            <Feed.Date>{calculateTime(notification.date)}</Feed.Date>
           </div>
-        </Feed.Content>
-      </Feed.Event>
+        </div>
+      </div>
+
       <Divider />
     </>
   );
