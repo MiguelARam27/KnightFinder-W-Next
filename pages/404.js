773da83dceef022e2571757049208964210415cd
MiguelARam27 <garandster27@live.com>
2021-12-15 00:53:40 -0500

    404 page css

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import styles from '@/styles/404.module.scss';
 export default function NotFoundPage() {
-  return <div>not found</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Page not found</h1>
+    </div>
+  );
 }
